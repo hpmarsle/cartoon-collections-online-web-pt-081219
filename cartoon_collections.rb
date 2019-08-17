e6a1d23a1962efbcd56053f7d36e7cd71b0bd44f
Mars Le <hp.mars.le@gmail.com>
2019-08-17 16:30:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,20 +15,20 @@ end
 def find_the_cheese(foods)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  i = 0
-  found_cheese = ''
-  while i < foods.length 
-    if cheese_types.include?(foods[i])
-      found_cheese = foods[i]
-      break
-    else
-      found_cheese = nil
-    end 
-    i += 1
-  end
-  found_cheese
-#.find 
-foods.find{|food| cheese_types.include?(food)}
+    #.find 
+  foods.find{|food| cheese_types.include?(food)}
+  # i = 0
+  # found_cheese = ''
+  # while i < foods.length 
+  #   if cheese_types.include?(foods[i])
+  #     found_cheese = foods[i]
+  #     break
+  #   else
+  #     found_cheese = nil
+  #   end 
+  #   i += 1
+  # end
+  # found_cheese
+
   
 end
